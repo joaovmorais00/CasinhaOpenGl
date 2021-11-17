e0b2a1c240cab1c1b95509f6ab045538cdf400ba
jaovmorais <joao.morais00@hotmail.com>
2021-11-17 14:34:39 -0300

    commit

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 Para compilar:
 
-1- acessar a pasta do projeto pelo terminal.
-2-Executar o comando: gcc joao_vitor_morais.c -o main -lGL -lglut -lGLEW
-3-Executar o comando ./main
+1- acessar a pasta em que o arquivo joao_vitor_morais.c está localizado.
+2- Executar o comando: gcc joao_vitor_morais.c -o main -lGL -lglut -lGLEW -lm
+3- Executar o comando ./main
--- joao_vitor_morais.c
+++ joao_vitor_morais.c
@@ -1,10 +1,14 @@
-#include<GL/freeglut.h>
+#include <GL/gl.h>
+#include <GL/glu.h>
+#include <GL/glut.h>
 #include<math.h>
 
-#define PI 3.1415926535
-
-void init(){
-    glClearColor(1,1,1,0);
+void init(void){
+    // glClearColor(1,1,1,0);
+    glClearColor (1.0,1.0,1.0,0.0);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(-1.0, 1.0, -1.0, 1.0, -1.0, 1.0);
 }
 
 void desenhaTriangulo(){
@@ -129,31 +133,89 @@ void desenhaPorta(){
     glEnd();
 }
 
-void desenhaLinhas(){
+void desenhaCirculo(){
+    float centroX= -0.3, centroY=0.2, r=0.09;
+    glBegin(GL_POLYGON);
+    glColor3f(0.4,0.6,1.0);
+    for(int i = 0; i < 360; i++)
+    {
+        float angulo = (i * M_PI)/180.0;
+        float x = centroX + (r * cos(angulo));
+        float y = centroY + (r * sin(angulo));
+        glVertex2f(x, y);
+
+    }
+    glEnd();
     glBegin(GL_LINE_LOOP);
+    glColor3f(0.0,0.0,0.0);
+    for(int i = 0; i < 360; i++)
+    {
+        float angulo = (i * M_PI)/180.0;
+        float x = centroX + (r * cos(angulo));
+        float y = centroY + (r * sin(angulo));
+        glVertex2f(x, y);
+
+    }
+    glEnd();
+    glBegin(GL_LINES);
     glColor3f(0,0,0);
+    glVertex2f(-0.3, 0.11);
+    glVertex2f(-0.3, 0.29);
+    glVertex2f(-0.39, 0.2);
+    glVertex2f(-0.21, 0.2);
+    glEnd();
+}
+
+void desenhaMacaneta(){
+    float centroX= -0.25, centroY=-0.4, r=0.02;
+    glBegin(GL_POLYGON);
+    glColor3f(0.0, 0.0,0.0);
+    for(int i = 0; i < 360; i++)
+    {
+        float angulo = (i * M_PI)/180.0;
+        float x = centroX + (r * cos(angulo));
+        float y = centroY + (r * sin(angulo));
+        glVertex2f(x, y);
+
+    }
     glEnd();
 }
 
 void desenha(void){
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glMatrixMode(GL_MODELVIEW);
+    glClear(GL_COLOR_BUFFER_BIT);
     desenhaTriangulo();
     desenhaParedes();
     desenhaTeto();
     desenhaJanelas();
     desenhaPorta();
-    glFlush();
+    desenhaMacaneta();
+    desenhaCirculo();
+    glutSwapBuffers();
 }
 
-void main(int argc, char** argv){
+void teclado (unsigned char key, int x, int y) {
+    switch (key)
+    {
+        case 27:             // ESCAPE key
+            exit (0);
+            break;
+    }
+}
+
+int main(int argc, char** argv){
     glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_DEPTH | GLUT_SINGLE|  GLUT_RGBA);
-    glutInitWindowSize(640, 480);
-    glutInitWindowPosition(100, 100);
+    // glutInitDisplayMode(GLUT_DEPTH | GLUT_SINGLE|  GLUT_RGBA);
+    // glutInitWindowSize(640, 480);
+    // glutInitWindowPosition(100, 100);
+    glutInitDisplayMode (GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+    glutInitWindowSize (640, 640); 
+    glutInitWindowPosition (100, 100);
     glutCreateWindow("Casinha 2d - Joao Vitor Morais");
     init();
     glutDisplayFunc(desenha);
+    glutKeyboardFunc(teclado);
     glutMainLoop();
-    return;
+    return 0;
 }
 
